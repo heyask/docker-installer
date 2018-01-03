533dfba62f66fe5d6eefa6c8ea5b714882a06974
Ubuntu <ubuntu@ip-172-31-14-65.ap-northeast-2.compute.internal>
2018-01-03 17:55:22 +0000

    fetch

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,50 +5,48 @@ if [ "$(. /etc/os-release; echo $NAME)" != "Ubuntu" ]; then
     exit 1
 fi
 
-if [ -x "$(command -v docker)" ]; then
-    echo 'Error: docker is already installed.' >&2
-    exit 1
+if ! [ -x "$(command -v docker)" ]; then
+    sudo apt-get remove docker docker-engine docker.io
+
+    sudo apt-get update -y
+
+    sudo apt-get install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        software-properties-common -y
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+    sudo apt-key fingerprint 0EBFCD88
+
+    sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+
+    sudo apt-get update -y
+
+    sudo apt-get install docker-ce -y
+
+    # sudo docker => docker
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+
+    # test
+    sudo docker run hello-world
+else
+    echo '- docker is already installed. skip.' >&2
 fi
 
 
-
-sudo apt-get remove docker docker-engine docker.io
-
-sudo apt-get update -y
-
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common -y
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo apt-key fingerprint 0EBFCD88
-
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-sudo apt-get update -y
-
-sudo apt-get install docker-ce -y
-
-# sudo docker => docker
-sudo groupadd docker
-sudo usermod -aG docker $USER
-
-# test
-sudo docker run hello-world
-
-
-
-if [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is already installed.' >&2
-  exit 1
+if ! [ -x "$(command -v docker-compose)" ]; then
+    sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    docker-compose --version
+else
+    echo '- docker-compose is already installed. skip.' >&2
 fi
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+
+echo '- complete!'
